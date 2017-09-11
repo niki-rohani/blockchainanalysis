69dcaae677b31a21d894d345b3d2db7d228de50e
niki <niki@clustaar.com>
2017-09-11 17:56:39 +0200

    new packages

---
+++ blockchain/lib/data/historical/poliniex_getter.py
@@ -0,0 +1,20 @@
+from . import Getter
+import quandl
+import numpy as np
+
+MARKET_KRAKEN="KRAKEN"
+MARKET_COINBASE='COINBASE'
+MARKET_BITSTAMP='BITSTAMP'
+MARKET_ITBIT='ITBIT'
+
+class QuandlGetter(Getter):
+
+
+    def __init__ (self, api_key):
+        quandl.ApiConfig.api_key = api_key
+
+    def _get(self, market, crypto_name=None):
+        quandl_id = "BCHARTS/"+market+"USD"
+        df = quandl.get(quandl_id, returns="pandas")
+        df = df.replace(0, np.nan)
+        return df
