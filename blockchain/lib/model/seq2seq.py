2068a2232ae3a7ab1b3a7452ffaddb1465b787d9
niki <niki@clustaar.com>
2017-09-14 17:03:42 +0200

    adding l2 loss

--- blockchain/lib/model/seq2seq.py
+++ blockchain/lib/model/seq2seq.py
@@ -1,11 +1,8 @@
 import tensorflow as tf
-from tensorflow.python.framework import dtypes
 from tensorflow.contrib import learn as tflearn
 from tensorflow.contrib.legacy_seq2seq.python.ops import seq2seq
-from tensorflow.python.framework import dtypes
 from tensorflow.contrib.learn.python.learn.ops import losses_ops
 from tensorflow.python.ops import variable_scope
-from tensorflow.python.summary import summary
 from blockchain.lib.nn import rnn
 from blockchain.lib.model.base_model import BaseModel
 
@@ -17,6 +14,8 @@ class Seq2Seq(BaseModel):
             model_fn=self._model(),
             model_dir=self.config["log_dir"]
         ))
+        self.bias = None
+        self.weights = None
 
 
     def _model(self):
@@ -24,67 +23,94 @@ class Seq2Seq(BaseModel):
         learning_rate = self.config["learning_rate"]
         optimizer = self.config["optimizer"]
 
-        def _modelfn(X, y):
+        def _modelfn(X, y, mode):
             # Create one LSTM cell (or multiple stacked LSTM as One cell step for our seq2seq)
             cell = rnn.lstm_cells(rnn_layers)
+
+
+            with variable_scope.variable_scope('linear_regression'):
+                dtype = tf.float32
+                output_shape = self.config["output_dim"]
+                self.weights = variable_scope.get_variable(
+                    'weights', [self.config["rnn_layers"][-1]["num_units"], output_shape], dtype=dtype)
+                self.bias = variable_scope.get_variable('bias', [output_shape], dtype=dtype)
 
             # from (batch_size x len_sequence x dim_input) to (len_sequence x batch_size x dim_input)
             x_ = tf.unstack(X, axis=1, num=X.shape[1])
 
 
-            # if we are in learning mode, y is not None
-            if y is not None:
-                # We create the decoder inputs, that are the outputs shift by 1 in the right and adding in first
-                # the "GO" symbol.
+            if mode != tf.contrib.learn.ModeKeys.INFER:
                 y_ = tf.unstack(y, axis=1, num=y.shape[1])
-                decoder_inputs = [tf.fill(y[-1].get_shape(), self.config["GO_SYMBOL"])]
-                for input in y_[:-1]:
-                    decoder_inputs.append(input)
-
-                loop_fn = None
             else:
                 y_ = None
-                decoder_inputs = [tf.fill([None,
-                                           self.config["output_dim"]], self.config["GO_SYMBOL"])]
 
-            decoder_inputs = y_
+            # if we are in learning mode, y is not None
+            if mode == tf.contrib.learn.ModeKeys.TRAIN:
+                # We create the decoder inputs, that are the outputs shift by 1 in the right and adding in first
+                # the "GO" symbol.
+                decoder_inputs = y_
+                decoder_inputs[1:] = y_[:-1]
+                decoder_inputs[0] = tf.zeros_like(decoder_inputs[0])
+                #[tf.fill(y[-1].get_shape(), self.config["GO_SYMBOL"])]
+                loop_fn = None
+            else:
+                batch_size = tf.shape(X)[0]
+                decoder_inputs = [tf.zeros([batch_size, self.config["output_dim"]])
+                                  for i in range(self.config["y_window"])]
+                def loop_fn(prev, i):
+                    predictions = tf.nn.xw_plus_b(prev, self.weights, self.bias)
+                  #  if len(predictions.get_shape()) == 2:
+                  #      predictions = seq2seq.array_ops.squeeze(predictions, squeeze_dims=[1])
+                    return predictions
+
 
             # We run the seq2seq
-            self.output, self.layers = rnn.attention_rnn_seq2seq(x_, decoder_inputs, cell)
+            self.output, self.layers = rnn.attention_rnn_seq2seq(x_, decoder_inputs, cell, loop_function=loop_fn)
 
             loss = 0
-            i = 0
+            reg = 0
             output_ = []
 
+            with variable_scope.variable_scope('label'):
+                if mode != tf.contrib.learn.ModeKeys.INFER:
+                    y_ = tf.unstack(y, axis=1, num=y.shape[1])
+
             with variable_scope.variable_scope('linear_regression'):
-                scope_name = variable_scope.get_variable_scope().name
-                dtype = self.output[0].dtype.base_dtype
-                output_shape = self.config["output_dim"]
-                weights = variable_scope.get_variable(
-                    'weights', [self.output[0].get_shape()[1], output_shape], dtype=dtype)
-                bias = variable_scope.get_variable('bias', [output_shape], dtype=dtype)
 
-                if y is not None:
-
+                if mode != tf.contrib.learn.ModeKeys.INFER:
                     for output, Y_ in zip(self.output, y_):
-                        prediction, l_ = losses_ops.mean_squared_error_regressor(output, Y_, weights, bias)
-                        output_.append(prediction)
-                        i+=1
+                        predictions = tf.nn.xw_plus_b(output, self.weights, self.bias)
+                        # if len(predictions.get_shape()) == 2:
+                        #       predictions = seq2seq.array_ops.squeeze(predictions, squeeze_dims=[1])
+                        print(predictions)
+                        print(Y_)
+                        l_ = tf.losses.mean_squared_error(Y_, predictions)
+                        output_.append(predictions)
                         loss += l_
+
                     self.output = output_
-                    loss = loss / (i+1)
+
+                    for tf_var in tf.trainable_variables():
+                        if not ("bias" in tf_var.name.lower() or "output_" in tf_var.name.lower()):
+                            reg += tf.reduce_mean(tf.nn.l2_loss(tf_var))
+
+                        loss = loss + self.config["l2"] * reg
                 else:
                     for output in self.output:
-                        predictions = tf.nn.xw_plus_b(output, weights, bias)
-                        if len(predictions.get_shape()) == 2:
-                            predictions = seq2seq.array_ops.squeeze(predictions, squeeze_dims=[1])
+                        predictions = tf.nn.xw_plus_b(output, self.weights, self.bias)
+                        # if len(predictions.get_shape()) == 2:
+                        #       predictions = seq2seq.array_ops.squeeze(predictions, squeeze_dims=[1])
                         output_.append(predictions)
 
                     self.output = output_
 
-            train_op = tf.contrib.layers.optimize_loss(
-                loss, tf.contrib.framework.get_global_step(), optimizer=optimizer,
-                learning_rate=learning_rate)
+            if mode == tf.contrib.learn.ModeKeys.TRAIN:
+                train_op = tf.contrib.layers.optimize_loss(
+                    loss, tf.contrib.framework.get_global_step(), optimizer=optimizer,
+                    learning_rate=learning_rate)
+            else:
+                train_op = None
+
             return self.output, loss, train_op
 
         return _modelfn
